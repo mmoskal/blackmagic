606108015089c80364fc1cc6edbcc74a43995e76
Michal Moskal <michal@moskal.me>
2019-02-15 14:20:58 -0800

    Adapt header file

--- src/include/uf2.h
+++ src/include/uf2.h
@@ -25,7 +25,6 @@ DEALINGS IN THE SOFTWARE.
 #ifndef UF2FORMAT_H
 #define UF2FORMAT_H 1
 
-#include "hw_config.h"
 #include "uf2cfg.h"
 
 #include <stdint.h>
@@ -44,6 +43,7 @@ DEALINGS IN THE SOFTWARE.
 #define UF2_IS_MY_FAMILY(bl)                                                                       \
     (((bl)->flags & UF2_FLAG_FAMILYID_PRESENT) == 0 || (bl)->familyID == UF2_FAMILY)
 
+#define BOARD_FLASH_SIZE (1024*1024)
 
 #define MAX_BLOCKS (BOARD_FLASH_SIZE / 256 + 100)
 typedef struct {
@@ -84,67 +84,5 @@ int write_block(uint32_t lba, const uint8_t *copy_from);
 int read_block(uint32_t block_no, uint8_t *data);
 void ghostfat_1ms(void);
 
-typedef void (*UF2_MSC_Handover_Handler)(UF2_HandoverArgs *handover);
-typedef void (*UF2_HID_Handover_Handler)(int ep);
-
-// this is required to be exactly 16 bytes long by the linker script
-typedef struct {
-    void *reserved0;
-    UF2_HID_Handover_Handler handoverHID;
-    UF2_MSC_Handover_Handler handoverMSC;
-    const char *info_uf2;
-} UF2_BInfo;
-
-#define UF2_BINFO ((UF2_BInfo *)(APP_START_ADDRESS - sizeof(UF2_BInfo)))
-
-static inline bool is_uf2_block(const void *data) {
-    const UF2_Block *bl = (const UF2_Block *)data;
-    return bl->magicStart0 == UF2_MAGIC_START0 && bl->magicStart1 == UF2_MAGIC_START1 &&
-           bl->magicEnd == UF2_MAGIC_END;
-}
-
-static inline bool in_uf2_bootloader_space(const void *addr) {
-    return USER_FLASH_END <= (uint32_t)addr && (uint32_t)addr < BOARD_FLASH_SIZE;
-}
-
-
-#ifdef UF2_DEFINE_HANDOVER
-static inline const char *uf2_info(void) {
-    if (in_uf2_bootloader_space(UF2_BINFO->info_uf2))
-        return UF2_BINFO->info_uf2;
-    return "N/A";
-}
-
-static inline void hf2_handover(uint8_t ep) {
-    const char *board_info = UF2_BINFO->info_uf2;
-    UF2_HID_Handover_Handler fn = UF2_BINFO->handoverHID;
-
-    if (in_uf2_bootloader_space(board_info) && in_uf2_bootloader_space((const void *)fn) &&
-        ((uint32_t)fn & 1)) {
-        // Pass control to bootloader; never returns
-        fn(ep & 0xf);
-    }
-}
-
-// the ep_in/ep_out are without the 0x80 mask
-// cbw_tag is in the same bit format as it came
-static inline void check_uf2_handover(uint8_t *buffer, uint32_t blocks_remaining, uint8_t ep_in,
-                                      uint8_t ep_out, uint32_t cbw_tag) {
-    if (!is_uf2_block(buffer))
-        return;
-
-    const char *board_info = UF2_BINFO->info_uf2;
-    UF2_MSC_Handover_Handler fn = UF2_BINFO->handoverMSC;
-
-    if (in_uf2_bootloader_space(board_info) && in_uf2_bootloader_space((const void *)fn) &&
-        ((uint32_t)fn & 1)) {
-        UF2_HandoverArgs hand = {
-            1, ep_in, ep_out, 0, cbw_tag, blocks_remaining, buffer,
-        };
-        // Pass control to bootloader; never returns
-        fn(&hand);
-    }
-}
-#endif
 
 #endif
---
+++ src/include/uf2cfg.h
@@ -0,0 +1,9 @@
+#define UF2_VERSION "1.0.0"
+#define BOARD_ID "Black-Magic-Probe"
+#define INDEX_URL "https://pxt.io/uf2"
+#define UF2_NUM_BLOCKS 8000
+#define VOLUME_LABEL "BLACKMAGIC"
+
+#define MSC_EP_IN 0x86
+#define MSC_EP_OUT 0x07
+#define INTF_MSC 5
